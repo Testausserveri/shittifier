1faacfb6488165c211886548cb9e93adb25d4f9c
DrVilepis <jarvinenville606@gmail.com>
2021-05-08 17:05:34 +0300

    Merge branch 'main' of https://github.com/DrVilepis/stupid-hack-2021

--- src/shittifiers/everythingTrue.js
+++ src/shittifiers/everythingTrue.js
@@ -2,6 +2,7 @@ export default function everythingTrue(data) {
     // true will become !!true
     // false will become !true
     return data
-        .replace(/true/gi, '!false')
-        .replace(/false/gi, '!true');
+        .replace(/([^a-q])(true)([^a-q])/gi, '$1!false$3')
+        .replace(/([^a-q])(false)([^a-q])/gi, '$1!true$3');
+
 }
