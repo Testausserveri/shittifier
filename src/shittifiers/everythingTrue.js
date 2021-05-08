b8327153cbe445816d42d10fde09170a534c5e4c
DrVilepis <jarvinenville606@gmail.com>
2021-05-08 17:17:41 +0300

    fix my shitass regex

--- src/shittifiers/everythingTrue.js
+++ src/shittifiers/everythingTrue.js
@@ -2,7 +2,6 @@ export default function everythingTrue(data) {
     // true will become !!true
     // false will become !true
     return data
-        .replace(/([^a-q])(true)([^a-q])/gi, '$1!false$3')
-        .replace(/([^a-q])(false)([^a-q])/gi, '$1!true$3');
-
+        .replace(/([^a-z|^0-9])(true)([^a-z|^0-9])/gi, '$1!false$3')
+        .replace(/([^a-z|^0-9])(false)([^a-z|^0-9])/gi, '$1!true$3');
 }
