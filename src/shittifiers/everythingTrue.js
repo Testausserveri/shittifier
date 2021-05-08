cfc15dc3a5d64b8679792a3e6f91849ee086ad68
DrVilepis <jarvinenville606@gmail.com>
2021-05-08 17:24:13 +0300

    fuck

--- src/shittifiers/everythingTrue.js
+++ src/shittifiers/everythingTrue.js
@@ -2,6 +2,6 @@ export default function everythingTrue(data) {
     // true will become !false
     // false will become !true
     return data
-        .replace(/([^a-z|^0-9])(true)([^a-z|^0-9])/gi, '$1!false$3')
-        .replace(/([^a-z|^0-9])(false)([^a-z|^0-9])/gi, '$1!true$3');
+        .replace(/([^a-z0-9])(true)([^a-z0-9])/gi, '$1!false$3')
+        .replace(/([^a-z0-9])(false)([^a-z0-9])/gi, '$1!true$3');
 }
