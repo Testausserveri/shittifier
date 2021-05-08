cf99c93ae507f3e32db236d44eb9b1de812d21ea
ahnl <46541386+ahnl@users.noreply.github.com>
2021-05-08 15:40:32 +0300

    Fix some error in the consoleLog shittifier

--- src/shittifiers/consoleLog.js
+++ src/shittifiers/consoleLog.js
@@ -1,5 +1,5 @@
 export default function consoleLog(data) {
     // console.log -> process.stdout.write
     // and add a new line with console.log()
-    return data.replace(/console\.log\((.*)\)/gi, "process.stdout.write($1);                              process.stdout.write('\\r\\n');").replace(/false/gi, '!true');
+    return data.replace(/console\.log\((.*)\)/gi, "process.stdout.write($1) \n                              process.stdout.write('\\r\\n')").replace(/false/gi, '!true');
 }
